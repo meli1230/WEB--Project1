321b3897c714181af99e75a9574463b7cbf238c4
meli1230 <melisa_marian@yahoo.fr>
2024-11-28 12:12:26 +0200

    initial header

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8"> <!--encoding type; enables the representation of text in any language-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--viewport controls how the page is displayed on mobile and responsive devices-->
+    <!--initial-scale=1.0 -> sets the default zoom level to 100% (no zoom) when the page loads-->
+    <title>Women Techpower Platform</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> <!--defines the bootstrap style from the source-->
+    <link rel="styesheet" href="css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="index.php">Women Techpower Platform</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="members.php">Members</a></li>
+                <li class="nav-item"><a class="nav-link" href="add_member.php">Add Member</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container mt-4"> <!--mt4 = margin-top of 4 units-->
