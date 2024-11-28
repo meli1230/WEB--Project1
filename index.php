63aa0a9faa5e6242762da041178fc8082921f314
meli1230 <melisa_marian@yahoo.fr>
2024-11-28 13:25:34 +0200

    initial index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include_once "includes/header.php";
+?>
+
+<div class="jumbotron">
+    <h1 class="display-4">Welcome to the Women Techpower Platform!</h1>
+    <p class="lead">Promotes women in the field of technology by providing educational resources, networking opportunities and professional development.</p>
+    <hr class="my-4"> <!--hr -> places a horizontal line (horizontal rule)-->
+                      <!--my4 -> applies a vertical margin (spacing) to an element-->
+    <p>Join our community of professional women in TechPower.</p>
+    <a class="btn btn-primary btn-lg" href="add_member.php" role="button">Join Now</a>
+</div>
+
+<?php
+include_once "includes/footer.php";
+?>
